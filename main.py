91a70eb484765130b52597001ce2fa7bc236cc60
Pakin Puttanukulchai <65996113+OnFireByte@users.noreply.github.com>
2022-04-24 18:46:14 +0700

    make default path redirect to random seed path

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Response
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, RedirectResponse
 
 import numpy as np
 import random
@@ -12,26 +12,21 @@ API_VERSION = "1.0.0"
 
 
 app = FastAPI()
+favicon_path = "favicon.ico"
 
 
 @app.get("/")
-def default():
-    return {"message": "You forgot to add a seed!"}
-
-
-@app.get("/test_file")
-def get_test_file():
-    return FileResponse("public/test.py")
+def go_to_random_seed():
+    random_seed = random.randint(0, 2 ** 32 - 1)
+    return RedirectResponse(url=f"/{random_seed}")
 
 
 @app.get("/{seed}")
-def get_random_test_by_seed(seed, response: Response):
+def get_random_test_by_seed(seed: int, response: Response):
     response.headers["api-version"] = API_VERSION
 
-    try:
-        seed = int(seed)
-    except ValueError:
-        return {"error": "seed must be an integer"}
+    if seed >= 2 ** 32 - 1:
+        return {"error": "seed is too large, maximum is 2**32 -1"}
 
     random.seed(seed)
     np.random.seed(seed)
@@ -43,6 +38,13 @@ def get_random_test_by_seed(seed, response: Response):
         "animate_drop": [random_animate_drop() for _ in range(TEST_COUNT)],
         "animate_clear": [random_animate_clear() for _ in range(TEST_COUNT)],
     }
+
+
+@app.get("/test_file")
+def serve_test_file():
+    return FileResponse("public/test.py")
+
+
 @app.get("/favicon.ico")
 async def serve_favicon():
     return FileResponse(favicon_path)
